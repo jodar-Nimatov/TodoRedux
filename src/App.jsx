a1297c589c7881754314a9f11475d95a9d30a844
jodar-Nimatov <115215766+jodar-Nimatov@users.noreply.github.com>
2023-01-31 00:40:57 +0600

    rem

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import { addTodo, completeTodo, removeTodo } from './redux/reducers/todo';
-// import {AiOutlineStar, AiFillStar} from 'react-icons/ai'
+import {AiOutlineStar, AiFillStar} from 'react-icons/ai'
+import {HiOutlinePencilAlt} from 'react-icons/hi'
 
 
 function App() {
@@ -34,12 +35,12 @@ function App() {
               <input type="checkbox" checked={todo.completed} onChange={() => dispatch(completeTodo(index))}/>
               <span>{item.title}</span>
             <button type='button' onClick={() => dispatch(removeTodo(index))}>Удалить</button>
-            {/* <span onClick={() => setImp(prev => !prev)}>
+            <span onClick={() => setImp(prev => !prev)}>
               {
                 !imp ? <AiOutlineStar/> : <AiFillStar/>
               }
-            </span> */}
-             <button onClick={()=>editing(item.title, index)}>✏️</button>
+            </span>
+             <button onClick={()=>editing(item.title, index)}><HiOutlinePencilAlt/></button>
             </li>
           ))}
         </ul>
